222a88c6df4188f1e89b9b04b087b5743e149654
unknown <drissaitkassi@gmail.com>
2023-12-04 13:33:50 +0000

    add support for windows terminal cmd  begin creating  .vscode folder and settings.json file

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const os=require('os')
+const fs=require('fs')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -34,14 +35,16 @@ function activate(context) {
 			case 'darwin':
 				// create zsh terminal instance 
 			console.log(" im on mac ")
-			let customTerminal=vscode.window.createTerminal('driss','/bin/zsh')
-			customTerminal.show()
+			vscode.window.createTerminal('driss','/bin/zsh').show()
+			
 				break;
 			case 'linux':
 			// create bash terminal instance 
+			console.log('im in linux ')
 				break;
 			case 'win32':
 				// create cmd terminal instance 
+			vscode.window.createTerminal('driss','C:\\WINDOWS\\system32\\cmd.exe').show()
 				break;
 			default:
 				break;
@@ -60,8 +63,92 @@ function activate(context) {
 
 
 		//check for .vscode folder and for settings.json file 
+		console.log('listing root dir ')
+		console.log(fs.readdirSync('C:\\Users\\HP\\Desktop\\test-extention'))
+
+		fs.readdirSync('C:\\Users\\HP\\Desktop\\test-extention').forEach(i=>{
+			if(i === '.vscode'){
+				console.log(' found it')
+
+				//vscode.window.activeTerminal.sendText('cd .vscode')
+
+				fs.readdirSync('C:\\Users\\HP\\Desktop\\test-extention\\.vscode').forEach(file=>{
+					if(file === 'settings.json'){
+
+						//check if file exclude settings are there 
+
+						console.log('found settings file ')
+					}else{
+
+						console.log('i didnt finde settings file')
+
+						vscode.window.activeTerminal.sendText('echo { >> settings.json')
+						vscode.window.activeTerminal.sendText('echo   "files.exclude": { >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/.git": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/.DS_Store": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/*.js": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/*.js.map": true >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         } >> settings.json')
+						vscode.window.activeTerminal.sendText('echo  } >> settings.json')
+						
+						let commandList=['echo { >> settings.json',
+						'echo   "files.exclude": { >> settings.json',
+						'echo         "**/.git": true, >> settings.json',
+						'echo         "**/.DS_Store": true, >> settings.json',
+						'echo         "**/*.js": true, >> settings.json',
+						'echo         "**/*.js.map": true >> settings.json',
+						'echo         } >> settings.json','echo  } >> settings.json']
+
+						//commandList.forEach(c=> vscode.window.activeTerminal.sendText(c))
+						
+							
+					}
+				})
+
+			}else{
+
+				
+				console.log("im on the else clause that creates the vs code folder")
+				vscode.window.activeTerminal.sendText('mkdir .vscode')
+				fs.readdirSync('C:\\Users\\HP\\Desktop\\test-extention\\.vscode').forEach(file=>{
+					if(file === 'settings.json'){
+
+						//check if file exclude settings are there 
+
+						console.log('found settings file ')
+					}else{
+
+						console.log('i didnt finde settings file')
+
+						vscode.window.activeTerminal.sendText('echo { >> settings.json')
+						vscode.window.activeTerminal.sendText('echo   "files.exclude": { >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/.git": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/.DS_Store": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/*.js": true, >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         "**/*.js.map": true >> settings.json')
+						vscode.window.activeTerminal.sendText('echo         } >> settings.json')
+						vscode.window.activeTerminal.sendText('echo  } >> settings.json')
+						
+						let commandList=['echo { >> settings.json',
+						'echo   "files.exclude": { >> settings.json',
+						'echo         "**/.git": true, >> settings.json',
+						'echo         "**/.DS_Store": true, >> settings.json',
+						'echo         "**/*.js": true, >> settings.json',
+						'echo         "**/*.js.map": true >> settings.json',
+						'echo         } >> settings.json','echo  } >> settings.json']
+
+						//commandList.forEach(c=> vscode.window.activeTerminal.sendText(c))
+						
+							
+					}
+				})
+				
+			}
+		})
+
 		//parse settings.json file looking for the exclude snippet if it does not exist 
 		//insert the exclude .js and .js map snippet and save document
+
 
 
 	vscode.workspace.onDidSaveTextDocument(
